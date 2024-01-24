a6bd3c6380cdf8758eb3099d952508045fdd2b1f
Sanyam <sanyamsharma.tech@gmail.com>
2024-01-24 17:11:37 +0530

    New js function

--- Array/index.js
+++ Array/index.js
@@ -1,15 +1,15 @@
 // How to find the largest number in array.
 
-// const array = [1, 3, 5, 7, 8, 9, 10, 200, 10928];
+const arrayData = [1, 3, 5, 7, 8, 9, 10, 200, 10928];
 
-// var largestElement = 0;
+let lElement = 0;
 
-// for (let i = 0; i < array.length; i++) {
-//   if (array[i] > largestElement) {
-//     largestElement = array[i];
-//   }
-// }
-// console.log(largestElement);
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > lElement) {
+    lElement = array[i];
+  }
+}
+console.log(lElement);
 
 // How to find the second largest element number in array.
 
@@ -29,3 +29,42 @@ for (let i = 0; i < array.length; i++) {
   }
 }
 console.log(secondLargest);
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// Brute Method
+
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+twoSum([2, 7, 11, 15], 9);
+
+// Optimal Force
+
+function twoSum(nums, target) {
+  const numsWithIndex = nums.map((num, index) => [num, index]);
+  numsWithIndex.sort((a, b) => a[0] - b[0]);
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    const sum = numsWithIndex[left][0] + numsWithIndex[right][0];
+    if (sum === target) {
+      return [numsWithIndex[left][1], numsWithIndex[right][1]];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return []; // No solution found!
+}
+
+twoSum([2, 7, 11, 15], 9);
